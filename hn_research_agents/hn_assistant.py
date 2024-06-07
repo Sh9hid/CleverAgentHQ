8c71e6bbaea1ad3bb3a3df49eb223319401c9ea0
Abdullah Shahid Khan <69131903+Sh9hid@users.noreply.github.com>
2024-06-07 23:58:44 +0530

    initial commit

---
+++ hn_research_agents/hn_assistant.py
@@ -0,0 +1,32 @@
+
+from phi.assistant import Assistant
+from phi.llm.ollama import Ollama
+from phi.tools.hackernews import HackerNews
+
+# Create instances of the Assistant
+story_researcher = Assistant(
+    name="HackerNews Story Researcher",
+    role="Researches hackernews stories and users.",
+    tools=[HackerNews()],
+    llm=Ollama(model="llama3:instruct", max_tokens=1024)
+)
+
+user_researcher = Assistant(
+    name="HackerNews User Researcher",
+    role="Reads articles from URLs.",
+    tools=[HackerNews()],
+    llm=Ollama(model="llama3:instruct", max_tokens=1024)
+)
+
+hn_assistant = Assistant(
+    name="Hackernews Team",
+    team=[story_researcher, user_researcher],
+    llm=Ollama(model="llama3:instruct", max_tokens=1024)
+)
+
+import json
+import httpx
+
+hn_assistant.print_response(
+    "Write a report about the users with the top 2 stories on hackernews", markdown=False
+)

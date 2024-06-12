fba1f37dd22f8130dd25ef6280823cff9855f089
Abdullah Shahid Khan <69131903+Sh9hid@users.noreply.github.com>
2024-06-12 23:49:06 +0530

    Update web-search-hq.py

--- web-search-hq/web-search-hq.py
+++ web-search-hq/web-search-hq.py
@@ -1,4 +1,3 @@
-
 from typing import List
 from pydantic import BaseModel, Field
 from phi.assistant import Assistant
@@ -6,20 +5,20 @@ from phi.llm.ollama import Ollama
 
 from phi.assistant import Assistant
 from phi.tools.serpapi_tools import SerpApiTools
+
 intern = Assistant(
     name = "intern",
     instruction="Find trending tutorials and tech articles and make a list of the top ones.",
     tool=[SerpApiTools()],
     llm=Ollama(model="llama3:instruct", max_tokens=1024)
 )
+
 analyst = Assistant(
     name= "analyst",
     instruction="Make a detailed report individually of each technical article provided to you.",
     tool=[SerpApiTools()],
     llm=Ollama(model="llama3:instruct", max_tokens=1024)
 )
-
-
 
 assistant = Assistant(
     name="personal assistant",
@@ -29,7 +28,6 @@ assistant = Assistant(
     show_tool_calls=True,
      llm=Ollama(model="llama3:instruct", max_tokens=1024),
 )
-
 
 assistant.print_response(
    "What's trending in tech?",
